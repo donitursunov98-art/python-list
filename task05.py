952c50ec4ea9fee594892e6b57dcc75fc49d0f6a
Abu <efootball042605@gmail.com>
2025-12-25 15:26:58 +0500

    5-mashq

---
+++ task05.py
@@ -0,0 +1,12 @@
+matritsa = [
+    [1, 2],
+    [3, 4]
+]
+
+print('2x2 jadval:')
+
+for qator in matritsa:
+    print('|', end=' ')
+    for element in qator:
+        print(element, end=' ')
+    print('|')
